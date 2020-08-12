f6a5e6251cb6cd532b8aaa952b548566c6d6844b
Vương Hữu Thiện <vuonghuuthientk@gmail.com>
2020-08-13 02:11:21 +0700

    Create Table

--- database.php
+++ database.php
@@ -6,16 +6,27 @@
     $DB_HOST = 'localhost'; // Host name //localhost
     $DB_USER = 'root'; // Host Username
     $DB_PASS = ''; // Host Password
-    $DB_NAME = 'test'; // Database name
+    $DB_NAME = 'myDB'; // Database name
     
     try {
         // PDO Connection
-        $conn = new PDO("mysql:host=$DB_HOST; dbname=$DB_NAME;", $DB_USER, $DB_PASS); 
-        // $conn = new PDO("sqlsrv:Server=$DB_HOST; Database=$DB_NAME", $DB_USER, $DB_PASS);
+        // $conn = new PDO("mysql:host=$DB_HOST; dbname=$DB_NAME;", $DB_USER, $DB_PASS); 
+        // $conn = new PDO("sqlsrv:Server=$DB_HOST; Database=$DB_NAME", $DB_USER, $DB_PASS); 
+
+        $conn = new PDO("mysql:host=$DB_HOST", $DB_USER, $DB_PASS);
         // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        // create Database if not exists
+        $dbs = $conn->query("CREATE DATABASE IF NOT EXISTS  $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"); 
+
+        $conn = new PDO("mysql:host=$DB_HOST; dbname=$DB_NAME;", $DB_USER, $DB_PASS); 
+        
+        echo "Connected successfully</br>";
     } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        echo "Connection failed: " . $e->getMessage() . "</br>";
     }
+
+    // 
+    $dbs = $conn->query("SHOW TABLES LIKE 'users'"); 
+    echo count($dbs->fetchAll());
 ?>
